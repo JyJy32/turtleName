706cb36e84f3b0b3eda4587fce89a41d34fcac01
Joren Liégeois <joren.liegeois@student.ap.be>
2021-11-24 16:55:09 +0100

    added comments

--- letterE.py
+++ letterE.py
@@ -6,19 +6,19 @@ def drawE(t=turtle.Turtle(), size=100, colour="blue"):
     t.pencolor(colour)
     t.pendown()
     t.forward(size)
-    t.backward(size)
+    t.backward(size) #_
     t.left(90)
-    t.forward(size*(3/5))
+    t.forward(size*(3/5)) #|
     t.right(90)
-    t.forward(size*(7/10))
+    t.forward(size*(7/10)) #-
     t.backward(size*(7/10))
     t.left(90)
-    t.forward(size*(3/5))
+    t.forward(size*(3/5))#|
     t.right(90)
     t.forward(size)
 
     t.penup()
-    t.setx(turtle.pos()[0]+ size)
+    t.sety(turtle.pos()[0]-(size*(3/5)))
 
 if __name__ == "__main__":
     drawE(t)
