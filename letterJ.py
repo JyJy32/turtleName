62eb984a76dbce957f205dd53b8babe24aa9a648
JyJy32 <joren.liegeois@gmail.com>
2021-11-26 14:58:31 +0100

    size parameter affects total height of drawing

--- letterJ.py
+++ letterJ.py
@@ -6,14 +6,14 @@ scrn = turtle.Screen()
 def drawJ(t, size=100, colour="blue"):
     t.penup()
     t.pencolor(colour)
-    t.sety(size/2)
+    t.sety(size*0.25)
     t.right(90)
     t.pendown()
-    t.circle(size/2, extent=180)
-    t.forward(size)
+    t.circle(size*0.25, extent=180)
+    t.forward(size*0.75)
     t.penup()
     t.right(90)
-    t.sety(t.pos()[1]-(1.5*size))
+    t.sety(t.pos()[1]-size)
 
 if __name__ == "__main__":
     drawJ(t)
