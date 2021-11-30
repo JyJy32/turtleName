9a6d8a6179a2cfc21ce9918ef14a8d670cc13930
Jakub-Smigalski <s131441@ap.be>
2021-11-30 12:59:54 +0100

    Added lowercase letter a

---
+++ letter_a.py
@@ -0,0 +1,46 @@
+import turtle # load the turtle module
+
+scrn = turtle.Screen() # Create/obtain the turtle screen object.
+bob = turtle.Turtle() # Create a turtle object, that we can use to draw. (I named it bob for some reason.)
+
+'''
+Copy this template for each letter. Then change the functions.
+The placeholders in this file just draw rectangles.
+'''
+def draw_letter_a(turtle, size=400):
+    '''Draw lowercase letter.'''
+    temppos = turtle.pos()
+    turtle.pensize(1)
+    turtle.setheading(0)
+    turtle.pencolor('grey')
+    turtle.penup()
+    turtle.forward(size/ 4)
+    turtle.left(90)
+    turtle.forward(size/ 10)
+    turtle.right(90)
+    turtle.pendown()
+    turtle.circle(size/ 8)
+    turtle.penup()
+    turtle.right(90)
+    turtle.forward(size/10)
+    turtle.left(90)
+    turtle.circle(size/ 4.44444, 135)# 90
+    turtle.pendown()
+    turtle.circle(size/ 4.44444, 270)
+    turtle.right(135)
+    turtle.forward(size/ 15.384615)
+    turtle.left(90)
+    for side in (size/ 11.11111, size/ 2.22222, size/ 11.11111):
+        turtle.forward(side)
+        turtle.left(90)
+    turtle.forward(size/ 15.384615)
+    turtle.penup()
+    turtle.setx(temppos[0] + size /2 + size / 40)
+    turtle.sety(temppos[1])
+
+
+    
+
+def draw_capital_letter_a(turtle, size=400):
+    '''Draw uppercase letter'''
+    draw_letter_a() # change this
