c5099804c7a3fc6966a51cc9bdbc7700913ac1ec
michiel lecat <michiel.lecat@ap.be>
2021-11-30 13:23:48 +0100

    qdsklf

--- LetterC.py
+++ LetterC.py
@@ -1,7 +1,7 @@
 import turtle  # load the turtle module
 
 
-def draw(t, size=400):
+def drawC(t, size=400):
     scrn = turtle.Screen()
     t = turtle.Turtle()
     t.penup()
@@ -18,19 +18,7 @@ if __name__ == "__main__":
     import turtle
     t = turtle.Turtle()
     scrn = turtle.Screen()
-    draw(t)
+    drawC(t)
     turtle.done()
 
 
-# def draw(turtle, size=400):
-#     '''Draw lowercase letter.'''
-#     turtle.pensize(1)
-#     turtle.setheading(0)
-#     turtle.pencolor('grey')
-#     turtle.pendown()
-#     for s in (1, 2):
-#         for side in (size / 2, size):
-#             turtle.forward(100)
-#             turtle.left(90)
-#     turtle.penup()
-#     turtle.setx(turtle.pos()[0] + size / 2 + size / 40)
