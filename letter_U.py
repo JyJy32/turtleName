216ca7f3cabe580e6bf8966b1975b4df80cb992f
jounes benktib <jounes.benktib@student.ap.be>
2021-11-30 13:54:38 +0100

    size

--- letter_U.py
+++ letter_U.py
@@ -2,23 +2,24 @@
 import turtle
 
 
-def drawU(bob):
+def drawU(bob, size=100):
 
     bob.pensize(1)
     bob.penup()
     bob.left(90)
-    bob.forward(150)
-    bob.right(180)
+    bob.forward(1.5*size)
+    bob.right(1.8*size)
     bob.pendown()
-    bob.forward(150)
-    bob.circle(45, 90)
-    bob.forward(50)
-    bob.circle(45, 90)
-    bob.forward(150)
+    bob.forward(1.5*size)
+    bob.circle(0.45*size, 90)
+    bob.forward(0.5*size)
+    bob.circle(0.45*size, 90)
+    bob.forward(1.5*size)
     bob.penup()
     bob.right(180)
-    bob.forward(190)
+    bob.forward(1.9*size)
     bob.left(90)
+    bob.forward(0.1*size)
 
 
 if __name__ == "__main__":
