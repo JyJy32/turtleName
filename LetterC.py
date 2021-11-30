44a81070a2db9dc8d7afefbc1d83a167937c847a
Cedric Schoofs <cedric.schoofs@student.ap.be>
2021-11-30 13:41:35 +0100

    final

--- LetterC.py
+++ LetterC.py
@@ -2,16 +2,18 @@ import turtle  # load the turtle module
 
 
 def draw(t, size=400):
-    scrn = turtle.Screen()
-    t = turtle.Turtle()
     t.penup()
     t.setx(t.pos()[0] + size / 2 + size / 40)
+    t.sety(size / 2)
     t.pendown()
     t.fd(size / 10)
     t.rt(size / 2.2)
     t.fd(size / 13)
     t.circle(size / 4, extent=180)
     t.fd(size / 10)
+    t.penup()
+    t.setx(size)
+    t.sety(size / 2)
 
 
 if __name__ == "__main__":
