e96a8c287c3d2e970d131da2705e74b3df8fea7b
jounes benktib <jounes.benktib@student.ap.be>
2021-12-03 14:21:50 +0100

    vhanges

--- letter_o.py
+++ letter_o.py
@@ -2,20 +2,24 @@
 import turtle
 
 
-def drawo(t):
+def drawo(t, size=100, colour="blue"):
 
     t.pensize(1)
     t.penup()
-    t.forward(45)
+    t.forward(size*0.50)
     t.pendown()
-    t.forward(45)
+    t.forward(size*0.25)
     t.circle(45, 90)
-    t.forward(150)
+    t.forward(size*1)
     t.circle(45, 90)
-    t.forward(45)
+    t.forward(size*0.25)
     t.circle(45, 90)
-    t.forward(150)
+    t.forward(size*1)
     t.circle(45, 90)
+
+    t.penup()
+    t.forward(size*0.80)
+    t.pendown()
 
 
 if __name__ == "__main__":
